042872e44d511e4d510333914fa029cbfc1477d4
sebaek <sebaek@gmail.com>
2023-08-28 12:24:00 +0900

    method

---
+++ src/ch05reference/C09method.java
@@ -0,0 +1,18 @@
+package ch05reference;
+
+public class C09method {
+	public static void main(String[] args) {
+		int a = 5;
+		
+		method1(a);
+		
+		int b = 50;
+		
+		method1(b);
+		
+	}
+	
+	public static void method1(int p) {
+		System.out.println(p);
+	}
+}
---
+++ src/ch05reference/C10method.java
@@ -0,0 +1,5 @@
+package ch05reference;
+
+public class C10method {
+
+}

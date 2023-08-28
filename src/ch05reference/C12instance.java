6e2c9187e668398d993b2b873f76a4c9d22c49e9
sebaek <sebaek@gmail.com>
2023-08-28 15:33:48 +0900

    객체와 클래스

---
+++ src/ch05reference/C12instance.java
@@ -0,0 +1,11 @@
+package ch05reference;
+
+public class C12instance {
+    public static void main(String[] args) {
+        // instance
+        // object
+        // 객체 :
+        //  1. 속성(값, 필드, property, attribute)
+        //  2. 기능(메소드, 함수, 액션)
+    }
+}
---
+++ src/ch06class/C01class.java
@@ -0,0 +1,33 @@
+package ch06class;
+
+public class C01class {
+    public static void main(String[] args) {
+        MyClass01 o1 = new MyClass01();
+        System.out.println("System.identityHashCode(o1) = " + System.identityHashCode(o1));
+
+        System.out.println("o1.age = " + o1.age);
+
+        o1.age = 30;
+
+        System.out.println("o1.age = " + o1.age);
+
+        MyClass01 o2 = o1;
+
+        System.out.println("o2.age = " + o2.age);
+        
+        o1.age = 60;
+
+        System.out.println("o2.age = " + o2.age);
+    }
+}
+
+class MyClass01 {
+    // instance fields
+    // 인스턴스가 가져야하는 속성들
+    int age;
+
+    // instance methods
+    // 인스턴스가 가져야하는 기능들
+
+
+}

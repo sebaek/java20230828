9e4da6376a20544696436ca7ddac59ed8177e29c
sebaek <sebaek@gmail.com>
2023-08-29 09:22:16 +0900

    instance field, static field

---
+++ src/ch06class/C05class.java
@@ -0,0 +1,29 @@
+package ch06class;
+
+public class C05class {
+    public static void main(String[] args) {
+        MyClass05 o1 = new MyClass05();
+        MyClass05 o2 = new MyClass05();
+
+        System.out.println("o1의 참조값" + System.identityHashCode(o1));
+
+        o1.age = 30;
+        o1.model = "java";
+
+        System.out.println("o1.age = " + o1.age); // 30
+        System.out.println("o2.age = " + o2.age); // 0
+        System.out.println("o1.model = " + o1.model); // java
+        System.out.println("o2.model = " + o2.model); // java
+    }
+}
+
+class MyClass05 {
+    // 속성 fields
+    // instance field
+    int age;
+
+    // static field, class field, 정적필드
+    static String model; 
+    
+    // 기능
+}

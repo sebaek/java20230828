effbad86c3d85c65b23d7c19edd7a9de4690fe0f
sebaek <sebaek@gmail.com>
2023-08-29 10:23:30 +0900

    static member에서는 instance member에 접근 불가능

---
+++ src/ch06class/C09static.java
@@ -0,0 +1,40 @@
+package ch06class;
+
+public class C09static {
+}
+
+class MyClass09 {
+    String name; // instance field
+    static String model; // static field
+
+    // instance method
+    void printName() {
+        // instance member끼리 접근 가능
+        System.out.println("name = " + name);
+
+        // instance member에서 static member 접근 가능
+        System.out.println("model = " + model);
+    }
+    
+    // static method
+    static void printModel() {
+        // static member끼리 접근 가능
+        System.out.println("model = " + model);
+        
+        // static member에서 instance member는 접근 불가능
+        System.out.println("name = " + name);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+

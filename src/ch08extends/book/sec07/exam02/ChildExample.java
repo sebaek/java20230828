dab4a210a225981322cbe8b9f0e5a8e2144f557b
sebaek <sebaek@gmail.com>
2023-08-29 16:20:09 +0900

    다형성

---
+++ src/ch08extends/book/sec07/exam02/ChildExample.java
@@ -0,0 +1,12 @@
+package ch08extends.book.sec07.exam02;
+
+public class ChildExample {
+    public static void main(String[] args) {
+        Child child = new Child();
+        Parent parent = child;
+
+        parent.method1();
+        parent.method2();
+//        parent.method3(); // x
+    }
+}

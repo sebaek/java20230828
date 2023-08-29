134e316c54d56eaf206a95b5c457912bf000d994
sebaek <sebaek@gmail.com>
2023-08-29 10:33:19 +0900

    getter setter

---
+++ src/ch06class/C10capsule.java
@@ -0,0 +1,27 @@
+package ch06class;
+
+public class C10capsule {
+}
+
+class MyClass10 {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}

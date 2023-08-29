99ec050b1ab3d80d8f4fda8a0453ca81b747d727
sebaek <sebaek@gmail.com>
2023-08-29 10:51:45 +0900

    상속

---
+++ src/ch08extends/lecture/C02extends.java
@@ -0,0 +1,16 @@
+package ch08extends.lecture;
+
+public class C02extends {
+    public static void main(String[] args) {
+        MySubClass021 o1 = new MySubClass021();
+        o1.name = "spring";
+        o1.method3();
+    }
+}
+
+// 위 코드에 문법오류가 없도록 MyClass02를 작성
+
+
+class MySubClass021 extends MyClass02 {
+
+}

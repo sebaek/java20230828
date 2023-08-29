14f526a541d7307a242528fb25b7b364444aa9ee
sebaek <sebaek@gmail.com>
2023-08-29 16:50:03 +0900

    polymorphism

---
+++ src/ch08extends/book/sec07/exam03/ChildExample.java
@@ -0,0 +1,19 @@
+package ch08extends.book.sec07.exam03;
+
+public class ChildExample {
+    public static void main(String[] args) {
+        Parent parent = new Child();
+
+        parent.field1 = "data1";
+        parent.method1();
+        parent.method2();
+        
+//        parent.field2 = "data2"; // 불가능
+//        parent.method3(); // 불가능
+
+        Child child = (Child) parent;
+
+        child.field2 = "data2";
+        child.method3();
+    }
+}

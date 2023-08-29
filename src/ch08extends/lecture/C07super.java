ca5e9fe2e93b46ff491ad658452d01bb9863eddc
sebaek <sebaek@gmail.com>
2023-08-29 14:10:33 +0900

    super

---
+++ src/ch08extends/lecture/C07super.java
@@ -0,0 +1,30 @@
+package ch08extends.lecture;
+
+public class C07super {
+    public static void main(String[] args) {
+        MySubClass071 o1 = new MySubClass071();
+        o1.method1();
+    }
+}
+
+class MyClass07 {
+    void method1() {
+        System.out.println("부모 클래스 메소드 기능");
+    }
+}
+
+class MySubClass071 extends MyClass07 {
+    @Override
+    void method1() {
+        super.method1(); // 부모 클래스의 메소드 호출
+        System.out.println("자식 클래스의 재정의한 메소드");
+    }
+}
+
+
+
+
+
+
+
+

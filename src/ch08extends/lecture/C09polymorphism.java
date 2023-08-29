6822fe92b3a51e054bdaf1e233cb022657d62c23
sebaek <sebaek@gmail.com>
2023-08-29 14:43:41 +0900

    polymorphism 다형성

--- src/ch08extends/lecture/C09polymorphism.java
+++ src/ch08extends/lecture/C09polymorphism.java
@@ -10,13 +10,26 @@ public class C09polymorphism {
 
         MyClass09 o3 = new MyClass09();
 //        MySubClass091 o4 = o3; // not ok
+
+        System.out.println(System.identityHashCode(o1));
+        System.out.println(System.identityHashCode(o2));
+        System.out.println(System.identityHashCode(o3));
+
+        o1.method1(); // 변수의 타입과 관계없이 실제 인스턴스의 메소드가 실행
+        o2.method1(); // 실제 인스턴스의 메소드 실행
+        o3.method1();
     }
 }
 
 class MyClass09 {
-
+    void method1() {
+        System.out.println("부모 메소드");
+    }
 }
 
 class MySubClass091 extends MyClass09 {
-
+    @Override
+    void method1() {
+        System.out.println("자식 메소드");
+    }
 }

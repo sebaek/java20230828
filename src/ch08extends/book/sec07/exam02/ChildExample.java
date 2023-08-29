9ade0a150f4f83c3a7e723ade89374b6a03b0b93
sebaek <sebaek@gmail.com>
2023-08-29 16:27:04 +0900

    다형성

--- src/ch08extends/book/sec07/exam02/ChildExample.java
+++ src/ch08extends/book/sec07/exam02/ChildExample.java
@@ -3,6 +3,12 @@ package ch08extends.book.sec07.exam02;
 public class ChildExample {
     public static void main(String[] args) {
         Child child = new Child();
+
+        child.method1();
+        child.method2();
+        child.method3();
+
+
         Parent parent = child;
 
         parent.method1();

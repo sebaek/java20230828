79603b049b064ce6882b2703bc652e420c96ff1c
sebaek <sebaek@gmail.com>
2023-08-29 09:26:20 +0900

    instance field, static field

--- src/ch06class/C05class.java
+++ src/ch06class/C05class.java
@@ -8,12 +8,13 @@ public class C05class {
         System.out.println("o1의 참조값" + System.identityHashCode(o1));
 
         o1.age = 30;
-        o1.model = "java";
+        o1.model = "java"; // 권장하지 않음.
+        MyClass05.model = "spring"; // 권장
 
         System.out.println("o1.age = " + o1.age); // 30
         System.out.println("o2.age = " + o2.age); // 0
-        System.out.println("o1.model = " + o1.model); // java
-        System.out.println("o2.model = " + o2.model); // java
+        System.out.println("o1.model = " + o1.model); // spring
+        System.out.println("o2.model = " + o2.model); // spring
     }
 }
 

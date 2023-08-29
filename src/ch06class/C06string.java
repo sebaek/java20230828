bd922c0f967f792df3e058e55e29ea2bdb12541d
sebaek <sebaek@gmail.com>
2023-08-29 09:36:05 +0900

    instance, static

---
+++ src/ch06class/C06string.java
@@ -0,0 +1,17 @@
+package ch06class;
+
+public class C06string {
+    public static void main(String[] args) {
+        String a = "java";
+        String b = "spring";
+
+        System.out.println(a.CASE_INSENSITIVE_ORDER);
+        System.out.println(b.CASE_INSENSITIVE_ORDER);
+        System.out.println(String.CASE_INSENSITIVE_ORDER);
+
+        System.out.println(a.length());
+        System.out.println(b.length());
+
+        System.out.println(String.valueOf(30));
+    }
+}

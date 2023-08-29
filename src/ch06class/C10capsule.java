2db99dce7c320dde4b763455288b9c4a5440ddf1
sebaek <sebaek@gmail.com>
2023-08-29 10:34:43 +0900

    getter setter

--- src/ch06class/C10capsule.java
+++ src/ch06class/C10capsule.java
@@ -4,6 +4,10 @@ public class C10capsule {
 }
 
 class MyClass10 {
+    // 특별한 이유가 없다면
+    // 필드는 private으로
+    // 관련된 메소드(주로 getter, setter)를 public으로 작성
+
     private int age;
     private String name;
 

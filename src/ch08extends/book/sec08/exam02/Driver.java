222283e1a3fd662b64b5dbe4ad749c8d551c49df
sebaek <sebaek@gmail.com>
2023-08-30 11:27:33 +0900

    다형성

---
+++ src/ch08extends/book/sec08/exam02/Driver.java
@@ -0,0 +1,7 @@
+package ch08extends.book.sec08.exam02;
+
+public class Driver {
+    public void drive(Vehicle vehicle) {
+        vehicle.run();
+    }
+}
---
+++ src/ch08extends/book/sec08/exam02/Vehicle.java
@@ -0,0 +1,7 @@
+package ch08extends.book.sec08.exam02;
+
+public class Vehicle {
+    public void run() {
+        System.out.println("차량이 달립니다.");
+    }
+}

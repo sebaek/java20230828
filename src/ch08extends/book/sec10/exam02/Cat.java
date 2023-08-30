0fdfc9b9b9995f16031dfb29f6c22bef0e5a1bb1
sebaek <sebaek@gmail.com>
2023-08-30 16:14:00 +0900

    문제10

---
+++ src/ch08extends/book/sec10/exam02/Cat.java
@@ -0,0 +1,9 @@
+package ch08extends.book.sec10.exam02;
+
+public class Cat extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("야옹");
+    }
+}
+
---
+++ src/ch08extends/book/sec10/exam02/Dog.java
@@ -0,0 +1,9 @@
+package ch08extends.book.sec10.exam02;
+
+public class Dog extends Animal {
+
+    @Override
+    public void sound() {
+        System.out.println("멍멍");
+    }
+}

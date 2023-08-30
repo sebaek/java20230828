fbf2b9bb87c1cea1f1ecbf5491d3fa2acf583b2f
sebaek <sebaek@gmail.com>
2023-08-30 15:29:06 +0900

    책예제

---
+++ src/ch08extends/book/sec10/exam01/PhoneExample.java
@@ -0,0 +1,11 @@
+package ch08extends.book.sec10.exam01;
+
+public class PhoneExample {
+    public static void main(String[] args) {
+        SmartPhone smartPhone = new SmartPhone("홍길동");
+
+        smartPhone.turnOn();
+        smartPhone.internetSearch();
+        smartPhone.turnOff();
+    }
+}

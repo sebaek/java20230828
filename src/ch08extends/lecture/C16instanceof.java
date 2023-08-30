32c6d32d8111c7254d9ae188a414ee77a3d7f330
sebaek <sebaek@gmail.com>
2023-08-30 09:48:16 +0900

    instanceof

--- src/ch08extends/lecture/C16instanceof.java
+++ src/ch08extends/lecture/C16instanceof.java
@@ -1,7 +1,32 @@
 package ch08extends.lecture;
 
 public class C16instanceof {
+    public static void main(String[] args) {
+        Animal16 ani1 = new Dog16();
+        Animal16 ani2 = new Cat16();
+        Animal16 ani3 = new Animal16();
 
+        // instanceof 연산자
+        // 연산결과 : true, false (boolean)
+        // 좌변 : 참조변수
+        // 우변 : 타입
+
+        boolean res1 = ani1 instanceof Animal16; // true;
+        boolean res2 = ani1 instanceof Dog16; // true
+        boolean res3 = ani1 instanceof Cat16; // false;
+
+        System.out.println("res1 = " + res1);
+        System.out.println("res2 = " + res2);
+        System.out.println("res3 = " + res3);
+
+        boolean res4 = ani2 instanceof Animal16;
+        boolean res5 = ani2 instanceof Dog16;
+        boolean res6 = ani2 instanceof Cat16;
+
+        boolean res7 = ani3 instanceof Animal16;
+        boolean res8 = ani3 instanceof Dog16;
+        boolean res9 = ani3 instanceof Cat16;
+    }
 }
 
 class Animal16 {}

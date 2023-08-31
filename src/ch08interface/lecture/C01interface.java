32fb7465b44269436772a42b75a57e8f976af2ee
sebaek <sebaek@gmail.com>
2023-08-31 10:45:52 +0900

    interface

---
+++ src/ch08interface/lecture/C01interface.java
@@ -0,0 +1,18 @@
+package ch08interface.lecture;
+
+public class C01interface {
+}
+
+interface MyInterface01 {
+    // static final 필드
+    // public 추상 메소드 (몸통이 없는 instance 메소드)
+    // public default 메소드 (몸통이 있는 instance 메소드)
+    // private 메소드(몸통이 있는 instace 메소드)
+    
+    // static public 메소드
+    // static private 메소드
+    
+}
+
+
+
---
+++ src/ch08interface/lecture/C02interface.java
@@ -0,0 +1,45 @@
+package ch08interface.lecture;
+
+public class C02interface {
+    public static void main(String[] args) {
+        MyClass021 o1 = new MyClass021();
+        MyInterface02 i2 = o1;
+
+        System.out.println(System.identityHashCode(o1));
+        System.out.println(System.identityHashCode(i2));
+
+        MyInterface02 i3 = new MyClass021();
+
+        System.out.println(System.identityHashCode(i3));
+
+        i3.method1();
+        i3.method2();
+
+    }
+}
+
+interface MyInterface02 {
+    // 추상 메소드
+    abstract public void method1();
+
+    void method2(); // public, abstract
+}
+
+class MyClass021 implements MyInterface02 {
+    @Override
+    public void method1() {
+        System.out.println("MyClass021.method1");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("MyClass021.method2");
+    }
+}
+
+
+
+
+
+
+

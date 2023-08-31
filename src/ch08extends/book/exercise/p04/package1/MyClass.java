8a9aa4108faa6be44fab6132f7c6d87efced123e
sebaek <sebaek@gmail.com>
2023-08-31 10:25:44 +0900

    override

--- src/ch08extends/book/exercise/p04/package1/MyClass.java
+++ src/ch08extends/book/exercise/p04/package1/MyClass.java
@@ -1,4 +1,16 @@
 package ch08extends.book.exercise.p04.package1;
 
 public class MyClass {
+
+    public void method1() {
+
+    }
+
+    protected void method2() {
+
+    }
+
+    protected void method3() {
+
+    }
 }
--- src/ch08extends/book/exercise/p04/package2/MySubClass.java
+++ src/ch08extends/book/exercise/p04/package2/MySubClass.java
@@ -3,4 +3,18 @@ package ch08extends.book.exercise.p04.package2;
 import ch08extends.book.exercise.p04.package1.MyClass;
 
 public class MySubClass extends MyClass {
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    protected void method2() {
+
+    }
+
+    @Override
+    public void method3() {
+
+    }
 }

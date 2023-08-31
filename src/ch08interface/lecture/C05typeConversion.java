b5523b4ee2aefe03661593943839b46fb17e1987
sebaek <sebaek@gmail.com>
2023-08-31 12:35:24 +0900

    자동 형변환

--- src/ch08interface/lecture/C05typeConversion.java
+++ src/ch08interface/lecture/C05typeConversion.java
@@ -1,10 +1,19 @@
 package ch08interface.lecture;
 
+import java.io.Serializable;
+import java.lang.constant.Constable;
+import java.lang.constant.ConstantDesc;
+
 public class C05typeConversion {
     public static void main(String[] args) {
         Integer i = 3;
 
         // 상위 타입으로 형변환 하는 코드 최대한 많이 작성해 보기
-        
+        Number n = i;
+        Object o = i;
+        Serializable s = i;
+        Comparable<Integer> c = i;
+        Constable co = i;
+        ConstantDesc cd = i;
     }
 }

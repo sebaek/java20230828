ff03a5e2d4d51598dcbbec71a18063f56f7c5042
sebaek <sebaek@gmail.com>
2023-09-01 10:05:25 +0900

    책 353 예제

---
+++ src/ch08interface/book/sec05/Television.java
@@ -0,0 +1,28 @@
+package ch08interface.book.sec05;
+
+public class Television implements RemoteControl {
+    private int volume;
+
+    @Override
+    public void turnOn() {
+        System.out.println("TV를 켭니다.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("TV를 끕니다.");
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        if (volume > RemoteControl.MAX_VOLUME) {
+            this.volume = RemoteControl.MAX_VOLUME;
+        } else if (volume < RemoteControl.MIN_VOLUME) {
+            this.volume = RemoteControl.MIN_VOLUME;
+        } else {
+            this.volume = volume;
+        }
+
+        System.out.println("현재 TV 볼륨: " + this.volume);
+    }
+}

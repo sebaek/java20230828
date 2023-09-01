dfd1f2f59c8a558249fe641e9fa062bda4638e37
sebaek <sebaek@gmail.com>
2023-09-01 14:38:59 +0900

    static nested class

---
+++ src/ch09nested/lecture/C02staticNested.java
@@ -0,0 +1,19 @@
+package ch09nested.lecture;
+
+public class C02staticNested {
+    public static void main(String[] args) {
+        MyClass02.NestedClass02 o1 = new MyClass02.NestedClass02();
+        
+    }
+}
+
+class MyClass02 {
+    static class NestedClass02 {
+        // 필드,생성자,메소드
+    }
+
+    void method1() {
+        NestedClass02 o2 = new NestedClass02();
+        
+    }
+}

c7b113f2db3229d367afab2ba78cf6344f03475e
sebaek <sebaek@gmail.com>
2023-09-04 11:50:23 +0900

    lambda

---
+++ src/ch16lambda/book/sec01/Calculable.java
@@ -0,0 +1,5 @@
+package ch16lambda.book.sec01;
+
+public interface Calculable {
+    void calculate(int x, int y);
+}
---
+++ src/ch16lambda/book/sec01/LambdaExample.java
@@ -0,0 +1,25 @@
+package ch16lambda.book.sec01;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        action(new Calculable() {
+            @Override
+            public void calculate(int x, int y) {
+                int result = x + y;
+                System.out.println("result = " + result);
+            }
+        });
+        
+        action(new Calculable() {
+            @Override
+            public void calculate(int x, int y) {
+                int result = x - y;
+                System.out.println("result = " + result);
+            }
+        });
+    }
+
+    public static void action(Calculable calculable) {
+
+    }
+}

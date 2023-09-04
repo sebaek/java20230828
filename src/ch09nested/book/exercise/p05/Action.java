a54410a9863b754c3a6a0361e80a6d639cf2cfcb
sebaek <sebaek@gmail.com>
2023-09-04 11:14:55 +0900

    확인문제 5

---
+++ src/ch09nested/book/exercise/p05/Action.java
@@ -0,0 +1,5 @@
+package ch09nested.book.exercise.p05;
+
+public interface Action {
+    public void work();
+}
---
+++ src/ch09nested/book/exercise/p05/ActionExample.java
@@ -0,0 +1,19 @@
+package ch09nested.book.exercise.p05;
+
+public class ActionExample {
+    public static void main(String[] args) {
+        Action action = new Action() {
+            @Override
+            public void work() {
+                System.out.println("복사를 합니다.");
+            }
+        };
+
+        action.work();
+
+        // lambda 
+        Action action2 = () -> System.out.println("복사를합니다.2");
+        action2.work();
+
+    }
+}

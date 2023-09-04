afbde3306dbbcdded956a0514e1da022fda1c50e
sebaek <sebaek@gmail.com>
2023-09-04 12:39:01 +0900

    파라미터 없는 추상메소드

---
+++ src/ch16lambda/book/exercise/p06/Button.java
@@ -0,0 +1,18 @@
+package ch16lambda.book.exercise.p06;
+
+public class Button {
+    @FunctionalInterface
+    public static interface ClickListener {
+        void onClick();
+    }
+
+    private ClickListener clickListener;
+
+    public void setClickListener(ClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+
+    public void click() {
+        this.clickListener.onClick();
+    }
+}

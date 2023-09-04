256bc97fd40a46cb9a2aa7af6fd468467e0c5420
sebaek <sebaek@gmail.com>
2023-09-04 11:27:44 +0900

    확인문제 7

---
+++ src/ch09nested/book/exercise/p07/Chatting.java
@@ -0,0 +1,25 @@
+package ch09nested.book.exercise.p07;
+
+public class Chatting {
+    class Chat {
+        void start() {}
+        void sendMessage(String message) {}
+    }
+
+    void startChat(String chatId) {
+        String nickName = null;
+        nickName = chatId;
+
+        Chat chat = new Chat() {
+            @Override
+            void start() {
+                while (true) {
+                    String inputData = "안녕하세요";
+                    String message = "[" + nickName + "]" + inputData;
+                    sendMessage(message);
+                }
+            }
+        };
+        chat.start();
+    }
+}

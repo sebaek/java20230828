10b96506579e42ed2f385f74801dea4f27e02597
sebaek <sebaek@gmail.com>
2023-09-04 10:25:10 +0900

    abstract method

--- src/ch09nested/lecture/C11interface.java
+++ src/ch09nested/lecture/C11interface.java
@@ -5,11 +5,25 @@ public class C11interface {
 //        MyInterface11 o1 = new MyInterface11();
         MyInterface11 o2 = new MyClass111();
         MyInterface11 o3 = new MyInterface11() {
+            @Override
+            public void method1() {
+                System.out.println("C11interface.method1");
+            }
         };
+
+        o3.method1();
     }
 }
 
 
-class MyClass111 implements MyInterface11 {}
+class MyClass111 implements MyInterface11 {
+    @Override
+    public void method1() {
+        System.out.println("MyClass111.method1");
+    }
+}
 
-interface MyInterface11 {}
+interface MyInterface11 {
+    void method1();
+}
+

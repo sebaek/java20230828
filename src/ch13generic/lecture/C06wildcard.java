6feec16d41037e920704b2b1b1e060d026d8a9ad
sebaek <sebaek@gmail.com>
2023-09-07 11:47:20 +0900

    wildcard

--- src/ch13generic/lecture/C06wildcard.java
+++ src/ch13generic/lecture/C06wildcard.java
@@ -12,10 +12,27 @@ public class C06wildcard {
         MyClass06<Dog> o1 = new MyClass06<>();
 //        MyClass06<Animal> o2 = o1; //?
 //        o2.set(new Animal());
+
         MyClass06<? extends Animal> o3 = o1;
+        MyClass06<? extends Animal> o6 = new MyClass06<Animal>();
+        MyClass06<? extends Animal> o7 = new MyClass06<Dog>();
+        MyClass06<? extends Animal> o8 = new MyClass06<Cat>();
+//        MyClass06<? extends Animal> o9 = new MyClass06<Object>(); // x
         Animal ani1 = o3.get(); // 안전
 //        o3.set(new Dog());// 안전 하지 않음
-        ani1.animalMethod();
+//        o6.set(new Cat());
+//        o7.set(new Dog());
+        ani1.animalMethod(); // 안전하게 사용하기 위함
+
+        MyClass06<? super Animal> o4 = new MyClass06<Animal>();
+        MyClass06<? super Animal> o5 = new MyClass06<Object>();
+//        MyClass06<? super Animal> o10 = new MyClass06<Dog>(); // x
+        o4.set(new Dog());
+        o5.set(new Dog());
+        o4.set(new Cat());
+        o4.set(new Animal());
+
+        
 
         
     }

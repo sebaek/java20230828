792c6a34ac390e3557ce372dc1322b97d621f0fa
sebaek <sebaek@gmail.com>
2023-09-08 10:58:56 +0900

    list

---
+++ src/ch99leetcode/Solution1046.java
@@ -0,0 +1,20 @@
+package ch99leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution1046 {
+    public int lastStoneWeight(int[] stones) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int stone : stones) {
+            list.add(stone);
+        }
+
+        // list
+        // sort, remove, add, size
+        
+
+        return 0;
+    }
+}
---
+++ src/ch99leetcode/Solution1046Test.java
@@ -0,0 +1,19 @@
+package ch99leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution1046Test {
+
+    @Test
+    void lastStoneWeight() {
+        Solution1046 o1 = new Solution1046();
+        int result1 = o1.lastStoneWeight(new int[]{2, 7, 4, 1, 8, 1});
+        assertEquals(1, result1);
+
+        int result2 = o1.lastStoneWeight(new int[]{1});
+        assertEquals(1, result2);
+
+    }
+}

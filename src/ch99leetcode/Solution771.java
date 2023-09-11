a9e1ac7ff2249087ef8c153f9736d812be7a2884
sebaek <sebaek@gmail.com>
2023-09-11 10:41:00 +0900

    set

--- src/ch99leetcode/Solution771.java
+++ src/ch99leetcode/Solution771.java
@@ -1,7 +1,16 @@
 package ch99leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution771 {
     public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewelSet = new HashSet<>();
+        for (char c : jewels.toCharArray()) {
+            jewelSet.add(c);
+        }
+        
+
         return 0;
     }
 }
--- src/ch99leetcode/Solution771Test.java
+++ src/ch99leetcode/Solution771Test.java
@@ -14,6 +14,6 @@ class Solution771Test {
 
         int j = o.numJewelsInStones("z", "ZZ");
         assertEquals(0, j);
-        
+
     }
 }

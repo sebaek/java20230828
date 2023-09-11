826d0565f14f11fc0d055c5ffab26185b1d00d25
sebaek <sebaek@gmail.com>
2023-09-11 12:18:17 +0900

    leetcode 1684

--- src/ch99leetcode/Solution1684.java
+++ src/ch99leetcode/Solution1684.java
@@ -1,7 +1,47 @@
 package ch99leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution1684 {
     public int countConsistentStrings(String allowed, String[] words) {
-        return 0;
+        // allowed : "ab"
+        Set<Character> chars = new HashSet<>();
+        for (char c : allowed.toCharArray()) {
+            chars.add(c);
+        }
+
+        // word : "ad"
+        chars.contains('a');
+        chars.contains('d');
+
+        // word : "badab"
+        chars.contains('b');
+        chars.contains('a');
+        chars.contains('d');
+        chars.contains('a');
+        chars.contains('b');
+
+        // word : "baa"
+        chars.contains('b');
+        chars.contains('a');
+        chars.contains('a');
+
+        int count = 0;
+        for (String word : words) {
+            boolean consistentString = true;
+            for (char c : word.toCharArray()) {
+                if (!chars.contains(c)) {
+                    consistentString = false;
+                    break;
+                }
+            }
+
+            if (consistentString) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }

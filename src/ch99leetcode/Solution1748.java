984b4a09270892ea49bd1b95e13d75b18f9511e4
sebaek <sebaek@gmail.com>
2023-09-11 15:30:42 +0900

    leetcode 1748 해결

--- src/ch99leetcode/Solution1748.java
+++ src/ch99leetcode/Solution1748.java
@@ -1,7 +1,37 @@
 package ch99leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution1748 {
     public int sumOfUnique(int[] nums) {
-        return 0;
+        // 1,2,3,2
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                // 이미 있으면
+                int oldValue = map.get(num);
+                map.put(num, oldValue + 1);
+            } else {
+                map.put(num, 1); // 첫 발견
+            }
+
+        }
+
+        // [1, 1]
+        // [2, 2]
+        // [3, 1]
+
+        int sum = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue().equals(1)) {
+                sum += entry.getKey();
+            }
+        }
+
+        System.out.println(map);
+
+        return sum;
     }
 }

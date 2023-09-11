e7b7128d636dcbcce5224fcbb9f3239eb086faac
sebaek <sebaek@gmail.com>
2023-09-11 10:38:46 +0900

    771 leetcode

---
+++ src/ch99leetcode/Solution771.java
@@ -0,0 +1,7 @@
+package ch99leetcode;
+
+public class Solution771 {
+    public int numJewelsInStones(String jewels, String stones) {
+        return 0;
+    }
+}
---
+++ src/ch99leetcode/Solution771Test.java
@@ -0,0 +1,19 @@
+package ch99leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution771Test {
+
+    @Test
+    void numJewelsInStones() {
+        Solution771 o = new Solution771();
+        int i = o.numJewelsInStones("aA", "aAAbbbb");
+        assertEquals(3, i);
+
+        int j = o.numJewelsInStones("z", "ZZ");
+        assertEquals(0, j);
+        
+    }
+}

5d9ce0c18d8a3d3f328d876c6ddcb752831755c6
sebaek <sebaek@gmail.com>
2023-09-12 15:16:03 +0900

    확인문제5

---
+++ src/ch17stream/book/sec05/exercise/p05/Example.java
@@ -0,0 +1,36 @@
+package ch17stream.book.sec05.exercise.p05;
+
+import java.util.List;
+
+public class Example {
+    public static void main(String[] args) {
+        var list = List.of(
+                "This is a java book",
+                "Lambda Expressions",
+                "Java8 supports lambda expressions"
+        );
+
+        System.out.println("고전적인 for 사용");
+        for (String item : list) {
+            if (item.toLowerCase().matches(".*java.*")) {
+                System.out.println(item);
+            }
+        }
+
+        System.out.println("stream 사용");
+        list.stream()
+                .filter(e -> e.toLowerCase().matches(".*java.*"))
+                .forEach(System.out::println);
+
+        System.out.println("contain 사용");
+        list.stream()
+                .filter(e -> e.toLowerCase().contains("java"))
+                .forEach(System.out::println);
+
+        System.out.println("indexOf 사용");
+        list.stream()
+                .filter(s -> s.toUpperCase().indexOf("JAVA") != -1)
+                .forEach(System.out::println);
+    }
+
+}

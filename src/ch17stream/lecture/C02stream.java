51e0b41e6629d59f202ea10821fee8388f4a749b
sebaek <sebaek@gmail.com>
2023-09-12 12:09:52 +0900

    stream

--- src/ch17stream/lecture/C02stream.java
+++ src/ch17stream/lecture/C02stream.java
@@ -9,10 +9,9 @@ public class C02stream {
 //        Stream<String> stream1 = stream.map(e -> e + e);
 //        long count = stream1.count();
         list.stream()
-                .map(e -> e+e) // 중간연산
+                .map(e -> e + e) // 중간연산
                 .filter(e -> e.length() > 1) // 중간연산
                 .count(); // 최종연산
-
 
 
     }

2fd775390b599695a8eccbe75e43fb0a38b58e28
sebaek <sebaek@gmail.com>
2023-09-12 14:03:52 +0900

    skip

---
+++ src/ch17stream/lecture/C10skip.java
@@ -0,0 +1,24 @@
+package ch17stream.lecture;
+
+import java.util.List;
+
+public class C10skip {
+    public static void main(String[] args) {
+        var list = List.of(10, 9, 1, 2, 5, 3);
+        list.stream()
+                .skip(3)
+                .forEach(System.out::println);
+
+        System.out.println("가장 작은값 출력");
+        list.stream()
+                .sorted()
+                .limit(1)
+                .forEach(System.out::println);
+
+        System.out.println("가장 큰 값 출력");
+        list.stream()
+                .sorted()
+                .skip(list.size() - 1)
+                .forEach(System.out::println);
+    }
+}

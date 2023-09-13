9290c5b31b416ee1504832ac19911f25e5b91032
sebaek <sebaek@gmail.com>
2023-09-13 09:44:00 +0900

    intstream,map,min,average

--- src/ch17stream/lecture/C19intStream.java
+++ src/ch17stream/lecture/C19intStream.java
@@ -1,15 +1,26 @@
 package ch17stream.lecture;
 
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
 public class C19intStream {
     public static void main(String[] args) {
-        IntStream.of(9, 10, 0, 3, 1, 22, 77, 33);
-
         // 최대값 구하기
-        // 최소값 구하기
-        // 평균 구하기
+        OptionalInt max = IntStream.of(9, 10, 0, 3, 1, 22, 77, 33)
+                .max();
+        System.out.println("max = " + max.getAsInt());
 
-        
+        // 최소값 구하기
+        OptionalInt min = IntStream.of(9, 10, 0, 3, 1, 22, 77, 33)
+                .min();
+        System.out.println("min = " + min.getAsInt());
+
+        // 평균 구하기
+        OptionalDouble average = IntStream.of(9, 10, 0, 3, 1, 22, 77, 33)
+                .average();
+        System.out.println("average = " + average.getAsDouble());
+
+
     }
 }

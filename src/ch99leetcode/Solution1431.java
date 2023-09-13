1f0eb22286730e819f1bbb215e1d91c503c9987e
sebaek <sebaek@gmail.com>
2023-09-13 12:36:13 +0900

    leetcode 1431

--- src/ch99leetcode/Solution1431.java
+++ src/ch99leetcode/Solution1431.java
@@ -1,17 +1,14 @@
 package ch99leetcode;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution1431 {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> list = new ArrayList<>();
-
         // 배열에서 최대값 구하기 [2, 3, 5, 1, 3]
-        int max = 0;
-        for (int candy : candies) {
-            max = Math.max(max, candy);
-        }
+        int max = Arrays.stream(candies)
+                .max()
+                .getAsInt();
 
         // extra와 더해서 max보다 크거나 같은지를 arraylist에 추가
         // [2, 3, 5, 1, 3]
@@ -20,11 +17,9 @@ public class Solution1431 {
         // list.add((5 + 3) >= 5); // true
         // list.add((1 + 3) >= 5); // false
         // list.add((3 + 3) >= 5); // true
-        for (int candy : candies) {
-            list.add((candy + extraCandies) >= max);
-        }
-
-
-        return list;
+        return Arrays.stream(candies)
+                .map(c -> c + extraCandies)
+                .mapToObj(c -> c >= max)
+                .toList();
     }
 }

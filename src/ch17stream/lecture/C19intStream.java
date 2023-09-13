e6f2a48957094e1a088afabaf7ef803457fe07b8
sebaek <sebaek@gmail.com>
2023-09-13 09:34:01 +0900

    intstream

---
+++ src/ch17stream/lecture/C19intStream.java
@@ -0,0 +1,15 @@
+package ch17stream.lecture;
+
+import java.util.stream.IntStream;
+
+public class C19intStream {
+    public static void main(String[] args) {
+        IntStream.of(9, 10, 0, 3, 1, 22, 77, 33);
+
+        // 최대값 구하기
+        // 최소값 구하기
+        // 평균 구하기
+
+        
+    }
+}

4ec0deed2245326532298dc988afdaa81e8d9485
sebaek <sebaek@gmail.com>
2023-09-14 10:27:14 +0900

    finally

---
+++ src/ch11exceptioin/book/sec02/exam02/sec02/exam01/ExceptionHandlingExample2.java
@@ -0,0 +1,22 @@
+package ch11exceptioin.book.sec02.exam02.sec02.exam01;
+
+public class ExceptionHandlingExample2 {
+    public static void printLength(String data) {
+        try {
+            int result = data.length();
+            System.out.println("문자수: " + result);
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("[마무리 실행]");
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("프로그램 시작");
+        printLength("ThisIsJava");
+        printLength(null);
+
+        System.out.println("프로그램 종료");
+    }
+}

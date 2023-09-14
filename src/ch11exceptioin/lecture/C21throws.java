6c41eb457036afa47513629fe81f427291e2e7f4
sebaek <sebaek@gmail.com>
2023-09-14 14:31:51 +0900

    throws Exception

---
+++ src/ch11exceptioin/lecture/C21throws.java
@@ -0,0 +1,11 @@
+package ch11exceptioin.lecture;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class C21throws {
+    public static void main(String[] args) throws FileNotFoundException {
+        var input = new FileInputStream("file");
+
+    }
+}
---
+++ src/ch11exceptioin/lecture/C22throws.java
@@ -0,0 +1,15 @@
+package ch11exceptioin.lecture;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class C22throws {
+    public static void main(String[] args) throws Exception {
+
+        Connection con = DriverManager.getConnection("", "", "");
+        var input = new FileInputStream("");
+        Class.forName("");
+        
+    }
+}

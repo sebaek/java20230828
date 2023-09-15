b161a15ad76eca6b2fa9547707b278af19e01c9e
sebaek <sebaek@gmail.com>
2023-09-15 16:47:05 +0900

    filters

--- src/ch18io/lecture/C22filter.java
+++ src/ch18io/lecture/C22filter.java
@@ -3,7 +3,7 @@ package ch18io.lecture;
 import java.io.*;
 
 public class C22filter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         InputStream is = new FileInputStream("");
         InputStreamReader isr = new InputStreamReader(is);
         BufferedReader br = new BufferedReader(isr);
@@ -11,7 +11,7 @@ public class C22filter {
         OutputStream os = new FileOutputStream("");
         BufferedOutputStream bos = new BufferedOutputStream(os);
         OutputStreamWriter osw = new OutputStreamWriter(bos);
-        
+
 
     }
 }

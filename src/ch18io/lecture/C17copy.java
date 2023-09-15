aed86414a4a4103cb4f4cd79ca1d6bd672b11504
sebaek <sebaek@gmail.com>
2023-09-15 15:20:38 +0900

    copy inputstream, outputstream

---
+++ src/ch18io/lecture/C17copy.java
@@ -0,0 +1,27 @@
+package ch18io.lecture;
+
+import java.io.*;
+
+public class C17copy {
+    public static void main(String[] args) {
+        String src = "src/ch18io/lecture/C17copy.java";
+        String des = "C:/Temp/C17copy.java";
+
+        try {
+            InputStream is = new FileInputStream(src);
+            OutputStream os = new FileOutputStream(des);
+
+            try (is;os;) {
+                byte[] buf = new byte[100];
+                int len = 0;
+                while ((len = is.read(buf)) != -1) {
+                    os.write(buf, 0, len);
+                }
+            }
+            os.flush();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

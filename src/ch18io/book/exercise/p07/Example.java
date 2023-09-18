debd57d843e1abd3e9d24ad994e41829ced699b7
sebaek <sebaek@gmail.com>
2023-09-18 10:24:18 +0900

    확인문제 7

---
+++ src/ch18io/book/exercise/p07/Example.java
@@ -0,0 +1,13 @@
+package ch18io.book.exercise.p07;
+
+public class Example {
+    public static void main(String[] args) {
+        String filePath = "src/ch18io/book/exercise/p07/Example.java";
+
+        // BufferedReader.readLine
+        // PrintStream.printf
+        
+
+
+    }
+}

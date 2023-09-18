cbcc16caa44450aa41c0f9c194dcf9a903ef45e7
sebaek <sebaek@gmail.com>
2023-09-18 16:34:35 +0900

    concurrency

---
+++ src/ch14thread/lecture/C10concurrency.java
@@ -0,0 +1,30 @@
+package ch14thread.lecture;
+
+public class C10concurrency {
+    private static int value = 0;
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000000; i++) {
+                value++;
+            }
+        });
+        t1.start();
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000000; i++) {
+                value++;
+            }
+        });
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("value = " + value);
+    }
+}

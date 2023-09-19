767e1dd0f9e90f0fa033db852563967b1869e69f
sebaek <sebaek@gmail.com>
2023-09-19 14:28:02 +0900

    쓰레드로 동시 처리

--- src/ch19network/lecture/C08server.java
+++ src/ch19network/lecture/C08server.java
@@ -7,32 +7,57 @@ import java.net.Socket;
 public class C08server {
     public static void main(String[] args) {
         // server가 client에게 파일 보내기
+        ServerSocket serverSocket = null;
         try {
 
-            ServerSocket serverSocket = new ServerSocket(5000);
-            Socket socket = serverSocket.accept();
+            serverSocket = new ServerSocket(5000);
 
-            String path = "C:/Temp/test.png";
-            InputStream is = new FileInputStream(path);
-            BufferedInputStream bis = new BufferedInputStream(is);
+            for (int i = 0; i < 30; i++) {
+                Socket socket = serverSocket.accept();
 
-            OutputStream os = socket.getOutputStream();
-            BufferedOutputStream bos = new BufferedOutputStream(os);
+                Thread t = new Thread(() -> {
+                    try {
 
-            try (bos; os; bis; is; socket; serverSocket) {
+                        String path = "C:/Temp/test.png";
+                        InputStream is = new FileInputStream(path);
+                        BufferedInputStream bis = new BufferedInputStream(is);
 
-                byte[] bytes = new byte[1024];
-                int len = 0;
+                        OutputStream os = socket.getOutputStream();
+                        BufferedOutputStream bos = new BufferedOutputStream(os);
 
-                while ((len = bis.read(bytes)) != -1) {
-                    bos.write(bytes, 0, len);
-                }
-                bos.flush();
+                        try (bos; os; bis; is;) {
 
-                System.out.println("서버가 파일을 보냈습니다 😜");
+                            byte[] bytes = new byte[1024];
+                            int len = 0;
+
+                            while ((len = bis.read(bytes)) != -1) {
+                                bos.write(bytes, 0, len);
+                            }
+                            bos.flush();
+
+                            System.out.println("서버가 파일을 보냈습니다 😜");
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+                t.start();
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }

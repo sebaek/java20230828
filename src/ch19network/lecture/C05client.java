a4a14ee7a6c1ac24b65a9903004f10b1350e5378
sebaek <sebaek@gmail.com>
2023-09-19 11:47:34 +0900

    network 파일 보내기

---
+++ src/ch19network/lecture/C05client.java
@@ -0,0 +1,38 @@
+package ch19network.lecture;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class C05client {
+    public static void main(String[] args) {
+        // client가 서버에게 파일 보내기
+
+        try {
+            Socket socket = new Socket("172.30.1.81", 3000);
+
+            String path = "C:/Temp/test.png";
+            FileInputStream fi = new FileInputStream(path);
+            BufferedInputStream bis = new BufferedInputStream(fi);
+
+            OutputStream os = socket.getOutputStream();
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+
+            try (socket; bos; os; bis; fi;) {
+                byte[] bytes = new byte[1024];
+                int length = 0;
+
+                while ((length = bis.read(bytes)) != -1) {
+                    bos.write(bytes, 0, length);
+                }
+                
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}

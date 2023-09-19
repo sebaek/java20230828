7f0cb6c35f3e417cc670c60ad222b64c76d4b13c
sebaek <sebaek@gmail.com>
2023-09-19 10:18:02 +0900

    synchronized method

--- src/ch14thread/lecture/C14synchronized.java
+++ src/ch14thread/lecture/C14synchronized.java
@@ -1,6 +1,34 @@
 package ch14thread.lecture;
 
 public class C14synchronized {
+    public static void main(String[] args) throws InterruptedException {
+        MyBox14 box = new MyBox14(0);
+        Thread t1 = new Thread(new Task14(box));
+        Thread t2 = new Thread(new Task14(box));
+        
+        t1.start();
+        t2.start();
+        
+        t1.join();
+        t2.join();
+
+        System.out.println("box.getValue() = " + box.getValue());
+    }
+}
+
+class Task14 implements Runnable {
+    private MyBox14 box;
+
+    public Task14(MyBox14 box) {
+        this.box = box;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            box.increment();
+        }
+    }
 }
 
 class MyBox14 {
@@ -10,7 +38,9 @@ class MyBox14 {
         this.value = value;
     }
 
-    public void increment() {
+    // synchronized method
+    public synchronized void increment() {
+        // monitor lock 은 this
         value++;
     }
 

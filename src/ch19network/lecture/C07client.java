f184f6acd7e28465e7ed3729faf8c30d7da18f5a
sebaek <sebaek@gmail.com>
2023-09-19 14:07:47 +0900

    server가 client에게 파일 보내기

---
+++ src/ch19network/lecture/C07client.java
@@ -0,0 +1,40 @@
+package ch19network.lecture;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class C07client {
+    public static void main(String[] args) {
+        // server가 client에게 파일 보내기
+
+        try {
+
+            Socket socket = new Socket("172.30.1.81", 5000);
+
+            InputStream is = socket.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+
+            String path = "C:/Temp/test-server.png";
+            FileOutputStream fos = new FileOutputStream(path);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            try (bos; fos; bis; is; socket) {
+
+                byte[] bytes = new byte[1024];
+                int len = 0;
+
+                while ((len = bis.read(bytes)) != -1) {
+                    bos.write(bytes, 0, len);
+                }
+                bos.flush();
+
+                System.out.println("클라이언트가 파일을 다 받음 😉");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

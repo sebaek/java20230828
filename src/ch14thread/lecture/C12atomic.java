4850776f4a1674bc1ddef4f85c5785b1f1f0d0a8
sebaek <sebaek@gmail.com>
2023-09-19 09:18:10 +0900

    thread safe

--- src/ch14thread/lecture/C12atomic.java
+++ src/ch14thread/lecture/C12atomic.java
@@ -1,29 +1,27 @@
 package ch14thread.lecture;
 
-public class C10concurrency {
-    private static int value = 0;
+import java.util.concurrent.atomic.AtomicInteger;
 
-    public static void main(String[] args) {
+public class C12atomic {
+    private static AtomicInteger value = new AtomicInteger();
+
+    public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 100000; i++) {
-                value++;
+                value.getAndIncrement();
             }
         });
         t1.start();
 
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 100000; i++) {
-                value++;
+                value.getAndIncrement();
             }
         });
         t2.start();
 
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        t1.join();
+        t2.join();
 
         System.out.println("value = " + value);
     }
